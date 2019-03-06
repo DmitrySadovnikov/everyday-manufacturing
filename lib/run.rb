8871781863183021499e949cd79e84411c304192
Dmitry Sadovnikov <sadovnikov.js@gmail.com>
2019-03-06 18:39:14 +0300

    Wrap run logic

--- lib/run.rb
+++ lib/run.rb
@@ -1,7 +1,9 @@
 require_relative 'frozen_thing'
 
-begin
-  FrozenThing::Customer.filter_by_distance.each { |c| puts "#{c.user_id} #{c.name}" }
-rescue FrozenThing::FileNotFound, InvalidFileContent, InvalidPointError => e
-  puts e
+module FrozenThing
+  begin
+    Customer.filter_by_distance.each { |c| puts "#{c.user_id} #{c.name}" }
+  rescue FileNotFoundError, InvalidFileContentError, InvalidPointError => e
+    puts e
+  end
 end
